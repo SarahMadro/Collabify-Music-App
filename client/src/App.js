58033183957d31ee24ed84a5d480f0a9eac30424
John <connollyjohnf@gmail.com>
2019-04-04 21:41:31 +0000

    turned NewPlaylist into component. Allowed state changes to move up into App, and passed props down to NPL

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import NewPlaylist from './newPlaylist';
 import SpotifyWebApi from 'spotify-web-api-js';
 const spotifyApi = new SpotifyWebApi();
 
@@ -19,9 +19,7 @@ class App extends Component {
       playlistDesc:'',
       playlistID:''
       };
-    this.createPlaylist = this.createPlaylist.bind(this)
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handlePLDescChange = this.handlePLDescChange.bind(this);
+
   }
 
   componentDidMount() {
@@ -46,52 +44,25 @@ class App extends Component {
     return hashParams;
   }
 
-  handleNameChange(event) {
-    let name = event.target.value;
+  getPLNameChange = (nameData) => {
     this.setState({
-      playlistName: name
-    });
-  }
-
-  handlePLDescChange(event) {
-    let playlistImage = event.target.value;
-    this.setState({
-      playlistImage: playlistImage
-    });
-  }
-
-  createPlaylist(e) {
-    e.preventDefault();
-    const { userId, playlistName, playlistDesc} = this.state;
-    const options = {
-      name: playlistName,
-      collaborative: true,
-      public: false,
-      description: playlistDesc
-    };
-    spotifyApi.createPlaylist(userId, options).then(response => {
-      console.log("PLAYLIST CREATED!");
-      this.setState({
-        playlistID: response.id
-      })
-    }, function(err){
-      console.log("Something went wrong!", err)
+      playlistName: nameData
     })
   }
 
+  getPLDescChange = (descData) => {
+    this.setState({
+      playlistDesc: descData
+    })
+  }
 
   render() {
     return (
       <div className='App'>
-        <a href='http://localhost:8080'> Login to Spotify</a><br />
-        {this.state.loggedIn && (
-          <form>
-            <label>Playlist Name: *</label>
-            <input defaultValue={'New Playlist'} onChange={this.handleNameChange} /><br />
-            <label>Playlist Description: </label>
-            <input placeholder='Enter description here...' onChange={this.handlePLDescChange} /> <br />
-            <button onClick={this.createPlaylist}>Create new Playlist!</button>
-          </form>
+      {!this.state.loggedIn && (
+    <a href='http://localhost:8080'> Login to Spotify</a>)}
+      {this.state.loggedIn && (
+        <NewPlaylist plInfo={this.state} getPLNameChange={this.getPLNameChange} getPLDescChange={this.getPLDescChange} />
         )}
       </div>
     );
