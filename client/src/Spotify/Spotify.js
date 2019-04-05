7464dc966c5b0ee638a1c3113d5f61d51e675a3c
Sarah <sjmadro1@gmail.com>
2019-04-05 12:39:10 -0700

    removing commented code

--- client/src/Spotify/Spotify.js
+++ client/src/Spotify/Spotify.js
@@ -20,35 +20,6 @@ const Spotify = {
     });
   },
 
-  // search(searchTerm) {
-  //   const accessToken = Spotify.getHashParams();
-  //   const headers = {
-  //     Authorization: `Bearer ${accessToken}`
-  //   };
-  //   return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, { headers: headers })
-  //     .then(
-  //       response => {
-  //         if (response.ok) {
-  //           return response.json();
-  //         }
-  //         throw new Error('Request failed!');
-  //       },
-  //       networkError => {
-  //         console.log(networkError.message);
-  //       }
-  //     )
-  //     .then(jsonResponse => {
-  //       if (!jsonResponse.tracks) {
-  //         return [];
-  //       }
-  //       return jsonResponse.tracks.items.map(track => ({
-  //         id: track.id,
-  //         name: track.name,
-  //         artist: track.artists[0].name,
-  //         album: track.album.name,
-  //         uri: track.uri
-  //       }));
-  //     });
 
   savePlaylist(playlistName, trackURIs) {
     if (playlistName && trackURIs.length) {
