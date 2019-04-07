3d5c0e15110147c8490faf56e7154f9027196981
Sarah <sjmadro1@gmail.com>
2019-04-06 18:28:01 -0700

    up to date with master this time for real

--- client/src/index.js
+++ client/src/index.js
@@ -6,22 +6,6 @@ import CreatePlaylist from './components/CreatePlaylist/CreatePlaylist';
 import Room from './components/Room/Room';
 
 const routing = (
-<<<<<<< HEAD
-  <Router >
-  <div>
-    <ul>
-      <li>
-        <Link to="/App">App</Link>
-      </li>
-      <li>
-        <Link to="/CreatPlaylist">CreatePlaylist</Link>
-      </li>
-    </ul>
-    <Route path='/' exact component={App}/>
-    <Route path='/CreatePlaylist' component={CreatePlaylist}/>
-    {/* <Route path='/Room' component={Room} /> */}
-  </div>
-=======
   <Router>
     <div>
       <ul>
@@ -36,7 +20,6 @@ const routing = (
       <Route path='/CreatePlaylist' component={CreatePlaylist} />
       <Route path='/Room' component={Room} />
     </div>
->>>>>>> fcd079646908a3d1a2e98ebb6044007048451a06
   </Router>
 );
 ReactDOM.render(routing, document.getElementById('root'));
