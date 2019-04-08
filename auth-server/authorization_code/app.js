4f808285bb69ba8c72b9fd624e274d6bb7207a9c
Sarah <sjmadro1@gmail.com>
2019-04-08 15:51:24 -0700

    nothing to commit

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -68,7 +68,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
   // your application requests authorization
   var scope =
-    'user-read-private user-read-playback-state playlist-modify-public playlist-modify-private playlist-read-collaborative';
+    'user-read-private user-read-playback-state playlist-modify-private playlist-read-collaborative';
   res.redirect(
     'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
